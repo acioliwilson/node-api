c73ec5d7898d00fe105434bccf5967261b5ccdd6
Wilson Acioli <acioliwilson@gmail.com>
2023-05-19 23:49:37 -0300

    cors resolve

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const axios = require('axios');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 
 // Defina o cabeçalho CORS, se necessário
-// app.use(cors());
+app.use(cors())
 
 app.get('/', async (req, res) => {
   try {
