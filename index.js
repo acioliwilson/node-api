13c6c58f69d818a88cf34c21ecf25ff36f9b7cad
Wilson Acioli <acioliwilson@gmail.com>
2023-06-06 20:53:47 -0300

    UPDATE

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// // Database Connection
-// const conn = require('./db/conn');
-// conn();
-
-// // API Routes
-// const routes = require('./routes/router');
-// app.use('/api', routes);
-
-// const port = 3000;
-// app.listen(port, () => {
-//     console.log('Running')
-// });
-
-
-
 const express = require('express');
 // const axios = require('axios');
 const cors = require('cors');
@@ -31,12 +9,8 @@ app.use(express.json());
 // Defina o cabeçalho CORS, se necessário
 app.use(cors());
 
-// Database Connection
-const conn = require('./db/conn');
-conn();
-
 app.get('/', async (req, res) => {
-  res.json({ msg: 'OK' });
+  res.send('Works');
 });
 
 const PORT = 3000;
