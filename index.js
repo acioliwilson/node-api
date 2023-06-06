68a84fe09f9303a2c343a2e4ebfdde4c8ab050f5
Wilson Acioli <acioliwilson@gmail.com>
2023-06-06 20:41:33 -0300

    UPDATE

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const express = require('express');
-// const axios = require('axios');
 const cors = require('cors');
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-// Defina o cabeçalho CORS, se necessário
-app.use(cors());
+// Database Connection
+const conn = require('./db/conn');
+conn();
 
-app.get('/', async (req, res) => {
-  res.send('Works');
+// API Routes
+const routes = require('./routes/router');
+app.use('/api', routes);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('Running')
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running ${PORT}`);
-});
 
-// Export the Express API
-module.exports = app
+
+// const express = require('express');
+// // const axios = require('axios');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(express.json());
+
+// // Defina o cabeçalho CORS, se necessário
+// app.use(cors());
+
+// app.get('/', async (req, res) => {
+//   res.send('Works');
+// });
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running ${PORT}`);
+// });
+
+// // Export the Express API
+// module.exports = app
