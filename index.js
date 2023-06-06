7159175fb11d7bb787ca320ea9c8f357d09aec63
Wilson Acioli <acioliwilson@gmail.com>
2023-06-06 20:32:42 -0300

    UPDATES

--- index.js
+++ index.js
@@ -10,16 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', async (req, res) => {
-  try {
-    // const response = await axios.get('https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest');
-    // res.json(response.data);
-    res.json({ msg: 'Works!' })
-  } catch (error) {
-    res.status(500).json({ error: 'Do not works' });
-  }
+  res.send('Works');
 });
 
-const PORT = proccess.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
 });
