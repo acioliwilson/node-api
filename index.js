92c075437598a5d3fd4ddf54269b23e5bce80956
Wilson Acioli <acioliwilson@gmail.com>
2023-06-06 20:49:29 -0300

    UPDATE

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Database Connection
+// const conn = require('./db/conn');
+// conn();
+
+// // API Routes
+// const routes = require('./routes/router');
+// app.use('/api', routes);
+
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log('Running')
+// });
+
+
+
 const express = require('express');
+// const axios = require('axios');
 const cors = require('cors');
+
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+// Defina o cabeçalho CORS, se necessário
+app.use(cors());
 
 // Database Connection
 const conn = require('./db/conn');
 conn();
 
-// API Routes
-const routes = require('./routes/router');
-app.use('/api', routes);
-
-const port = 3000;
-app.listen(port, () => {
-    console.log('Running')
+app.get('/', async (req, res) => {
+  res.json({ msg: 'OK' });
 });
 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
 
-
-// const express = require('express');
-// // const axios = require('axios');
-// const cors = require('cors');
-
-// const app = express();
-
-// app.use(express.json());
-
-// // Defina o cabeçalho CORS, se necessário
-// app.use(cors());
-
-// app.get('/', async (req, res) => {
-//   res.send('Works');
-// });
-
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running ${PORT}`);
-// });
-
-// // Export the Express API
-// module.exports = app
+// Export the Express API
+module.exports = app
